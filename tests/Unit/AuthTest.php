7be3c1dbab9f78a6b45f56cd89715bdb864095dc
Hamza GBOURI <hamzagbouri2004@gmail.com>
2025-03-12 13:44:29 +0000

    install phpunit

---
+++ tests/Unit/AuthTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Unit;
+
+
+
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+//    public function test_login_with_invalid_credentials()
+//    {
+//        $response = $this->postJson('/api/login', [
+//            'email' => 'user@example.com',
+//            'password' => 'password123'
+//        ]);
+//
+//        $response->assertStatus(401)
+//            ->assertJsonStructure([
+//                'error'
+//            ]);
+//    }
+//    public function test_login_with_valid_credentials()
+//    {
+//        $response = $this->postJson('/api/login', [
+//            'email' => 'hamzagbouri2004@gmail.com',
+//            'password' => 'mamababa123'
+//        ]);
+//
+//        $response->assertStatus(200)
+//            ->assertJsonStructure([
+//                'token'
+//            ]);
+//    }
+
+//    public function test_login_with_invalid_email()
+//    {
+//        $response = $this->postJson('/api/login', [
+//            'email' => 'invalid-email',
+//            'password' => 'password123'
+//        ]);
+//
+//        $response->assertStatus(401)
+//        ->assertJsonStructure(['error']);
+//    }
+    public function test_register_with_valid_credentials()
+    {
+        $response = $this->postJson('/api/register', [
+            'email' => 'hamza.gbouri@gmail.com',
+            'password' => 'mamababa123',
+            'name' => 'Hamza'
+        ]);
+        $response->assertStatus(201)
+            ->assertJsonStructure(['token']);
+    }
+//    public function test_register_with_invalid_credentials()
+//    {
+//        $response = $this->postJson('/api/register', [
+//            'email' => 'hamzagbouri2004@gmail.com',
+//            'password' => 'mamababa123',
+//            'name' => 'Hamza'
+//        ]);
+//        $response->assertStatus(400);
+//
+//
+//    }
+}
---
+++ tests/Unit/ItineraireTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ItineraireTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_data_if_entered_correctly()
+    {
+        $this->assertTrue(true);
+    }
+}
